6959f1e03ae74d7b834594f2074976e49c5e7739
satvik9934 <87279121+satvik9934@users.noreply.github.com>
2023-12-22 00:09:29 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Hosted Link : https://movie-react-app-av.netlify.app/
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import Main from './Components/Main/Main'
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
